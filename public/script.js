a3da1083c55619c3b101146c7412da4ba7b61216
nuf1k <marten@jalakas.ee>
2022-10-11 22:01:07 +0300

    made getProducts and postProducts functions for frontend to see & update products on the page

--- public/script.js
+++ public/script.js
@@ -1,15 +1,32 @@
-fetch('/products', {
+function postProducts( name, brand, price ) {
+    fetch('/products', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-        name: 'Brown sofa',
-        brand: 'MASKU',
-        price: 600,
+        name: name,
+        brand: brand,
+        price: price,
     }),
 }).then(res => {
-    if (res.ok) return res.json()
+    if (res.ok) return res.json();
     return "Failed to update product"
-    })
-    .then(data => console.log(data))
+    }).then(data => console.log(data));
+
+}
+
+const products = document.getElementById("products");
+
+function getProducts() {
+    fetch('/products', {
+        method: 'GET',
+    }).then(res => {
+        if (res.ok) return res.text();
+        return "Failed to fetch products";
+    }).then(text => {
+        products.innerHTML = text;
+    });
+};
+
+getProducts()
