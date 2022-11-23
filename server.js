7b68c13665e9e7ecc8fb75a839e2da18edf71400
Marten <marten@jalakas.ee>
2022-11-23 11:48:20 +0000

    fixed error where PM2 didn't get correct static site reference

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const pimRoutes = require('./src/pumpim/routes')
 const app = express();
 const port = 8080;
 
-app.use(express.static("./public"));
+app.use(express.static(__dirname + "/public"));
 
 app.use(
   express.urlencoded({
